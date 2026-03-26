aceef7c6910ea7a1ba2991cc91b4f887618cab07
tokha <khathtb01844@gmail.com>
2026-03-26 19:09:37 +0000

    Mar 27, 2026, 2:09 AM

--- obj/Debug/net10.0/COTHUYPRO.AssemblyInfo.cs
+++ obj/Debug/net10.0/COTHUYPRO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COTHUYPRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53d453c5751038f9357b507fe8eac84af8fb167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8003924c64e768702096cb045033c8638328dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("COTHUYPRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COTHUYPRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
